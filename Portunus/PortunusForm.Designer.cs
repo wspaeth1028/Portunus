86d0849d2b5062fd2bb37fb9e5ba10721bc447bc
wspaeth <will.spaeth@gmail.com>
2018-03-09 16:30:38 -0700

    added opaque panel class

--- Portunus/PortunusForm.Designer.cs
+++ Portunus/PortunusForm.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pnlDropHere = new ExtendedPanel();
+            this.lblDropHere = new System.Windows.Forms.Label();
+            this.pnlDropHere.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox1
@@ -39,18 +42,43 @@
             this.textBox1.TabIndex = 0;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
+            // pnlDropHere
+            // 
+            this.pnlDropHere.AllowDrop = true;
+            this.pnlDropHere.Controls.Add(this.lblDropHere);
+            this.pnlDropHere.Location = new System.Drawing.Point(12, 12);
+            this.pnlDropHere.Name = "pnlDropHere";
+            this.pnlDropHere.Opacity = 70;
+            this.pnlDropHere.Size = new System.Drawing.Size(260, 238);
+            this.pnlDropHere.TabIndex = 1;
+            this.pnlDropHere.Visible = false;
+            // 
+            // lblDropHere
+            // 
+            this.lblDropHere.AllowDrop = true;
+            this.lblDropHere.AutoSize = true;
+            this.lblDropHere.Location = new System.Drawing.Point(112, 105);
+            this.lblDropHere.Name = "lblDropHere";
+            this.lblDropHere.Size = new System.Drawing.Size(75, 13);
+            this.lblDropHere.TabIndex = 0;
+            this.lblDropHere.Text = "Drop File Here";
+            this.lblDropHere.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // PortunusForm
             // 
             this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.pnlDropHere);
             this.Controls.Add(this.textBox1);
             this.Name = "PortunusForm";
             this.Text = "Portunus";
-            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.OnDragEnter);
             this.DragDrop += new System.Windows.Forms.DragEventHandler(this.OnDragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.OnDragEnter);
             this.DragLeave += new System.EventHandler(this.OnDragLeave);
+            this.pnlDropHere.ResumeLayout(false);
+            this.pnlDropHere.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -59,6 +87,8 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
+        private ExtendedPanel pnlDropHere;
+        private System.Windows.Forms.Label lblDropHere;
     }
 }
 
--- Portunus/PortunusForm.cs
+++ Portunus/PortunusForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,16 +22,23 @@ namespace Portunus
         private void OnDragEnter(object sender, DragEventArgs e)
         {
             this.textBox1.Text = "Farts";
+            this.pnlDropHere.Visible = true;
+            string[] filelist = e.Data.GetData(DataFormats.FileDrop) as string[];
+            for (int i = 0; i < filelist.Length; i++)
+            {
+                //filelist.
+            }
         }
 
         private void OnDragDrop(object sender, DragEventArgs e)
         {
-
+            string[] fileList = e.Data.GetData(DataFormats.FileDrop) as string[];
         }
 
         private void OnDragLeave(object sender, EventArgs e)
         {
             this.textBox1.Text = "";
+            this.pnlDropHere.Visible = false;
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
